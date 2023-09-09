54a2382082e846c5bbe0c78d77adfe571d59593d
Julia Marchenko <yuliamarche@gmail.com>
2023-09-09 23:49:33 +0300

    Add DemoFormTest autotest

---
+++ src/test/java/DemoFormTest.java
@@ -0,0 +1,63 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class DemoFormTest {
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.holdBrowserOpen = false;
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = "1920x1080";
+    }
+
+    @Test
+    void fillFormTest() {
+        open("https://demoqa.com/automation-practice-form");
+        executeJavaScript("$('#fixedban').remove()");
+        executeJavaScript("$('footer').remove()");
+        $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
+
+        $("#firstName").setValue("Julia");
+        $("#lastName").setValue("Ivanova");
+        $("#userEmail").setValue("Ivanova@mail.com");
+        $(byText("Female")).click();
+        $("#userNumber").setValue("9187832345");
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-dropdown-container").$(byText("June")).click();
+        $(".react-datepicker__year-select").selectOption("1994");
+        $(".react-datepicker__day--019").click();
+        $("#subjectsInput").setValue("Computer Science").pressEnter();
+        $(byText("Reading")).click();
+        $("#uploadPicture").uploadFromClasspath("Ivanova.jpg");
+        $("#currentAddress").setValue("Currect Address");
+        $("#state").click();
+        $("#stateCity-wrapper").$(byText("Haryana")).click();
+        $("#city").click();
+        $("#stateCity-wrapper").$(byText("Karnal")).click();
+        $("#submit").click();
+
+        $(".modal-dialog").should(text("Thanks for submitting the form"));
+        $(".modal-content").shouldHave(
+                text("Julia Ivanova"),
+                text("Ivanova@mail.com"),
+                text("Female"),
+                text("9187832345"),
+                text("19 June,1994"),
+                text("Computer Science"),
+                text("Ivanova.jpg"),
+                text("Currect Address"),
+                text("Haryana Karnal")
+        );
+    }
+}
+
+
+
+
+
+
